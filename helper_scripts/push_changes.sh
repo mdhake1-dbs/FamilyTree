a0e74a95a2c66f74837ca22de615900fe041dd7c
mdhake1-dbs <mdhake1-dbs@users.noreply.github.com>
2025-11-24 20:25:14 +0000

    Updated ansible playbook to Configure DB Setup

--- helper_scripts/push_changes.sh
+++ helper_scripts/push_changes.sh
@@ -20,7 +20,7 @@ fi
 
 # Stage, commit, and push
 git add .
-git commit -m "Updated ansible playbook to install sqlserver prerequisites"
+git commit -m "Updated ansible playbook to Configure DB Setup"
 git branch -M main
 git push -u origin main
 
