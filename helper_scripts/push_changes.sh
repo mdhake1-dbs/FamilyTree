1359fb1968a00b2a2ba703f428ca4b8ad26eab0c
mdhake1-dbs <mdhake1-dbs@users.noreply.github.com>
2025-11-24 20:09:24 +0000

    Updated ansible playbook to install sqlserver prerequisites

--- helper_scripts/push_changes.sh
+++ helper_scripts/push_changes.sh
@@ -20,7 +20,7 @@ fi
 
 # Stage, commit, and push
 git add .
-git commit -m "Updated Sql Script"
+git commit -m "Updated ansible playbook to install sqlserver prerequisites"
 git branch -M main
 git push -u origin main
 
