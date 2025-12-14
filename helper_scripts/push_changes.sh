32c28f8f42add5eb20484d4ded5ac465bdbef878
mdhake1-dbs <mdhake1-dbs@users.noreply.github.com>
2025-12-14 01:02:12 +0000

    Removed SQL DB and Migrations from ansible

--- helper_scripts/push_changes.sh
+++ helper_scripts/push_changes.sh
@@ -20,7 +20,7 @@ fi
 
 # Stage, commit, and push
 git add .
-git commit -m "Updated Python Backend to apply SQL Migrations and avoid it from CI-CD and Ansible"
+git commit -m "Removed SQL DB and Migrations from ansible"
 git branch -M main
 git push -u origin main
 
